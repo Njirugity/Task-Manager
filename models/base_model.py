26373a5368ece45f4a783bc2de501c6775ed00f2
brian <muriithibrian283@gmail.com>
2025-07-01 07:57:10 +0300

    Create dictonary verison of object

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from sqlalchemy import Column, String, Integer, DateTime
 from sqlalchemy.orm import declarative_base
 import uuid
-
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
 Base = declarative_base()
@@ -17,3 +16,16 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = self.created_at
+    def save (self):
+        from storage.db import Db
+        db = Db()
+        return db
+    def to_dict(self):
+        new_dict = self.__dict__.copy()
+        if "created_at" in new_dict:
+            new_dict["created_at"] = new_dict["created_at"].strftime(time)
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        new_dict["__class__"] = self.__class__.__name__
+
+        return new_dict
