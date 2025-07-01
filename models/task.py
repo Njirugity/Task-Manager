33ffbdba0386a9a4263dd29989137fcd3ee17492
brian <muriithibrian283@gmail.com>
2025-07-01 07:54:52 +0300

    Add relationship on tasks and user

--- models/task.py
+++ models/task.py
@@ -1,17 +1,21 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from models.base_model import Base, BaseModel
 
 class Tasks(BaseModel, Base):
     __tablename__ = "tasks"
-    title = Column(String, nullable=False)
-    description = Column(String, nullable=True)
+
+    title = Column(String(256), nullable=False)
+    description = Column(String(500), nullable=True)
     completed = Column(Boolean, default=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
 
-    def __init__(self,title, description,completed,user_id, *args, **kwargs):
+    userRlt = relationship('User', back_populates="taskRlt")
+
+    def __init__(self,title, description,user_id, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.title = title
         self.description = description
-        self.completed = completed
+        
         self.user_id = user_id
 
